2040953b2dc31b2291612ec46af43aa0d0f5a186
alexquar <alexanderquarrie@gmail.com>
2024-03-25 16:38:49 -0400

    dynamic modal style

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,9 @@
 import './Modal.css'
 import ReactDOM from 'react-dom'
-export default function Modal({children, handleClose}) {
+export default function Modal({children, handleClose, isSalesModal}) {
   return ReactDOM.createPortal ((
     <div className="modal-backdrop">
-      <div className="modal" style= {{border: "4px solid", borderColor : '#ffd900', textAlign: "center"}}>
+      <div className="modal" style= {{border: "4px solid", borderColor: isSalesModal ? '#ffd900'  : '#555', textAlign: "center"}}>
         <h2>   
         {children}
         </h2>
