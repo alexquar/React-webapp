01d885afa2e987c8ca63c99d181e29665124f87e
alexquar <alexanderquarrie@gmail.com>
2024-03-25 13:58:56 -0400

    conditional templates and previous state

--- src/App.js
+++ src/App.js
@@ -2,31 +2,39 @@
   import './App.css';
 
   function App() {
-  const [name, setName] = useState('mario')
+    const [showEvents, setShowEvents] = useState(true)
   const [events, setEvents] = useState([
     {title: "mario's birthday bash", id: 1},
     {title: "bowser's live stream", id: 2},
     {title: "race on moo moo farm", id: 3}
   ])
-    const handleClick = () => {
-          setName('luigi')   
-    }
 
-    return (
-      <div className="App">
-      <h1> {name}</h1>
-      <button onClick={handleClick}>Change Name </button>
-      {
-        events.map((event)=>(
-          <div key={event.id}> 
-            <h2>
-              {event.title}
-            </h2>
-            </div>
-        ))
-      }
-      </div>
-    );
+  const handleClick = (id) => {
+    setEvents(prevEvents => {
+      return prevEvents.filter(event => id !== event.id)
+    })
   }
+
+  return (
+    <div className="App">
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>Hide Events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>Show Events</button>
+        </div>
+      )}
+      {showEvents && events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
+        </div>
+      ))}
+    </div>
+  );
+}
 
   export default App;
