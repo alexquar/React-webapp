679aa30c79274a43954cfa813547ccfe7fdd8e32
alexquar <alexanderquarrie@gmail.com>
2024-03-25 16:30:14 -0400

    inline styles

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 export default function Modal({children, handleClose}) {
   return ReactDOM.createPortal ((
     <div className="modal-backdrop">
-      <div className="modal">
+      <div className="modal" style= {{border: "4px solid", borderColor : '#ffd900', textAlign: "center"}}>
         <h2>   
         {children}
         </h2>
