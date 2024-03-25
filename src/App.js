70c78448947e9dcdab68e1aff0ae913ca15aab28
alexquar <alexanderquarrie@gmail.com>
2024-03-25 13:17:56 -0400

    loops

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@
 
   function App() {
   const [name, setName] = useState('mario')
-
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
+    {title: "race on moo moo farm", id: 3}
+  ])
     const handleClick = () => {
           setName('luigi')   
     }
@@ -12,6 +16,15 @@
       <div className="App">
       <h1> {name}</h1>
       <button onClick={handleClick}>Change Name </button>
+      {
+        events.map((event)=>(
+          <div key={event.id}> 
+            <h2>
+              {event.title}
+            </h2>
+            </div>
+        ))
+      }
       </div>
     );
   }
