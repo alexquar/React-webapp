9d9ed80e979fe72edda049051ae318d7c470f886
alexquar <alexanderquarrie@gmail.com>
2024-03-26 11:19:37 -0400

    adding locations

--- src/components/newEventForm.jsx
+++ src/components/newEventForm.jsx
@@ -3,15 +3,18 @@ import './newEventForm.css'
 export default function NewEventForm({addEvent}) {
     const [title,setTitle] = useState('')
     const [date,setDate] = useState('')
+    const [location,setLocation] = useState('Waterloo')
     const resetForm = () =>{
         setTitle('')
         setDate('')
+        setLocation('Waterloo')
     }
     const handleSubmit = (e) => {
         e.preventDefault()
         const event = {
           title: title,
           date: date,
+          location: location,
           id: Math.floor(Math.random() * 10000)
         }
         addEvent(event)
@@ -26,6 +29,14 @@ export default function NewEventForm({addEvent}) {
       <label>
         <span> Event Date: </span>
       <input type="date" onChange={ (e)=> setDate(e.target.value)} value={date}/>
+      </label>
+      <label>
+        <span>Event Location:</span>
+        <select onChange={(e) => setLocation(e.target.value)}>
+          <option value="Waterloo">Waterloo</option>
+          <option value="Cambridge">Cambridge</option>
+          <option value="Kitchener">Kitchener</option>
+        </select>
       </label>
      <button>Submit</button>
      <p onClick={resetForm}>Reset</p>
